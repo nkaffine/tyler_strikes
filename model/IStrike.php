18dd74fd040a79779d9b06704b05bb884b3f1dcc
nkaffine <nicholas.kaffine@gmail.com>
2018-02-18 15:45:26 -0500

    Initial Commit

---
+++ model/IStrike.php
@@ -0,0 +1,15 @@
+<?php
+
+    /**
+     * Created by PhpStorm.
+     * IUser: Nick
+     * Date: 2/18/18
+     * Time: 3:40 PM
+     */
+
+    /**
+     * Interface representing a strike against tyler.
+     */
+    interface IStrike {
+
+    }
